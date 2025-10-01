2369ae94ce00ee0f716c03bd9f9a7a2cb476d118
iqbal5354 <80057549+iqbal5354@users.noreply.github.com>
2025-10-01 23:42:25 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,10 +1,7 @@
 import time
 from bullove import bullove
 
-@bullove(pattern=r"\.ping$")
+@bullove(pattern=r"\.ping")
 async def _(e):
-    start = time.time()
-    xx = await e.respond("🏓 Pong...")
-    end = time.time()
-    ms = int((end - start) * 1000)
-    await xx.edit(f"🏓 Pong!\n⏱ {ms} ms")
+    print("✅ Event .ping terdeteksi")  # debug log ke Railway
+    await e.respond("🏓 Pong!")
